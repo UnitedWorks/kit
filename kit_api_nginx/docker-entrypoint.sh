04d8a8921c7d4417040723c7b284a177d93c9af2
Mark Hansen <x@markthemark.com>
2016-12-18 13:19:13 -0500

    Init. Contains nginx configurations for both api and dashboards. Contains configuraiton JSON for AWS tasks, services, and other production-related infastructure

---
+++ kit_api_nginx/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Exit the script as soon as something fails.
+set -e
+
+# Forwarding name and port. Should be the name of the Docker image that is linked to nginx.
+# To test locally, set to "kit_api_1"
+PLACEHOLDER_FORWARDING_NAME="api"
+PLACEHOLDER_FORWARDING_PORT="5000"
+
+# The server_name value
+# If using AWS load balancer, typically a EC2 instance's public DNS name
+# If not, set to the domain name (ex: foo.com)
+# To test locally, set to "localhost"
+PLACEHOLDER_VHOST="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
+
+# Default config location (set in Dockerfile)
+DEFAULT_CONFIG_PATH="/etc/nginx/conf.d/default.conf"
+
+# Replace all instances of the placeholders with the values above.
+sed -i "s/PLACEHOLDER_VHOST/${PLACEHOLDER_VHOST}/g" "${DEFAULT_CONFIG_PATH}"
+sed -i "s/PLACEHOLDER_FORWARDING_NAME/${PLACEHOLDER_FORWARDING_NAME}/g" "${DEFAULT_CONFIG_PATH}"
+sed -i "s/PLACEHOLDER_FORWARDING_PORT/${PLACEHOLDER_FORWARDING_PORT}/g" "${DEFAULT_CONFIG_PATH}"
+
+# Execute the CMD from the Dockerfile and pass in all of its arguments.
+exec "$@"
